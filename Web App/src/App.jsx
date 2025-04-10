a031b86724165c3bed26fe50dbcb9581d6b5e744
JonathanVarild <jonathan.varild@gmail.com>
2025-04-10 13:49:43 +0200

    Added navbar and a page view

--- Web App/src/App.jsx
+++ Web App/src/App.jsx
@@ -1,10 +1,22 @@
-import AuthenticatePresenter from "./presenters/AuthenticatePresenter";
 import NavbarPresenter from "./presenters/NavbarPresenter";
+import { TAB_ALARM, TAB_STATISTICS, TAB_RECORDINGS, TAB_SETTINGS } from "./model/interface/navigation";
+import { useSelector } from "react-redux";
+import PageView from "./views/PageView";
 
 function App() {
+	const activeTab = useSelector((state) => state.interface.navigationTab);
+
+	// Define the pages for each tab
+	const pages = [];
+	pages[TAB_ALARM] = <PageView title="Alarm">This is the alarm page...</PageView>;
+	pages[TAB_STATISTICS] = <PageView title="Statistics" />;
+	pages[TAB_RECORDINGS] = <PageView title="Recordings" />;
+	pages[TAB_SETTINGS] = <PageView title="Settings" />;
+
 	return (
 		<>
-			<AuthenticatePresenter />
+			{pages[activeTab]}
+			<NavbarPresenter />
 		</>
 	);
 }
