88cc91f00bf00b407103cec5dc6b88da9fc66f98
JonathanVarild <jonathan.varild@gmail.com>
2025-04-10 14:12:59 +0200

    fix: Replace index with tab.id for map key

--- Web App/src/views/NavbarView.jsx
+++ Web App/src/views/NavbarView.jsx
@@ -6,7 +6,7 @@ function NavbarView(props) {
 		<div className="flex flex-row justify-evenly gap-2 border-t-1 pb-5">
 			{props.tabs.map((tab, index) => (
 				<Button
-					key={index}
+					key={tab.id}
 					variant="ghost"
 					className={clsx("flex flex-col items-center h-auto ", props.activeTab === tab.id ? "text-primary" : "text-muted-foreground")}
 					onClick={() => props.changeTab(tab.id)}
