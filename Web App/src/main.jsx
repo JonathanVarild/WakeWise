64c30a6ff517d493360a65aea92d5eee4f4e7ab8
JonathanVarild <jonathan.varild@gmail.com>
2025-04-10 13:49:43 +0200

    Added navbar and a page view

--- Web App/src/main.jsx
+++ Web App/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import "./extra.css"
+import App from "./App.jsx";
 import { Provider } from "react-redux";
-import model from "./model/";
+import model from "./model/index.js";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Provider store={model}>
 			<App />
