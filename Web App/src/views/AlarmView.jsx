6b505e3b0829b6a9bc20981d2d2d649a3459c902
floralindberg <floral@kth.se>
2025-04-14 23:02:36 +0200

    More complex UI
    Not finished, minor bug where the user can input letters. Need to fix so that the user can only input numbers

--- Web App/src/views/AlarmView.jsx
+++ Web App/src/views/AlarmView.jsx
@@ -3,90 +3,97 @@ import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { setHoursOfSleep, setWakeUpTime } from "../model/alarmSlice"; // Importera Redux actions
-import { TimePicker } from "react-ios-time-picker";
-
+import { setHoursOfSleep, setWakeUpTime } from "../model/alarmSlice";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { MobileTimePicker } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
 
 function AlarmView() {
   const dispatch = useDispatch();
 
-  // Hämta värden från Redux state
   const hoursOfSleep = useSelector((state) => state.alarm.hoursOfSleep);
   const wakeUpTime = useSelector((state) => state.alarm.wakeUpTime);
 
-  // Hantera ändring av väckningstid
-  const handleTimeChange = (timeValue) => {
-    dispatch(setWakeUpTime(timeValue)); // Uppdatera Redux state med ny tid
-  };
-
-  // Funktion för att öka timmar
-  const increaseSleep = () => {
-    dispatch(setHoursOfSleep(hoursOfSleep + 0.5)); // Öka med 0.5 timmar
-  };
-
-  // Funktion för att minska timmar
-  const decreaseSleep = () => {
-    dispatch(setHoursOfSleep(hoursOfSleep - 0.5)); // Minska med 0.5 timmar
-  };
-
-  // Funktion för att hantera direkt inmatning i input-fältet
-  const handleInputChange = (e) => {
-    const inputValue = parseFloat(e.target.value); // Konvertera till ett flyttal
-    if (!isNaN(inputValue) && inputValue >= 0 && inputValue <= 12) {
-      dispatch(setHoursOfSleep(inputValue)); // Uppdatera Redux state
+  const handleTimeChange = (newValue) => {
+    if (newValue) {
+      const formattedTime = dayjs(newValue).format("HH:mm");
+      dispatch(setWakeUpTime(formattedTime));
     }
   };
 
-  const handleTimePickerFocus = () => {
-    document.body.style.overflow = "hidden"; // Inaktivera sidans scrollning
-  };
-  
-  const handleTimePickerBlur = () => {
-    document.body.style.overflow = "auto"; // Återställ sidans scrollning
+  const increaseSleep = () => {
+    dispatch(setHoursOfSleep(hoursOfSleep + 0.5));
   };
 
+ 
+  const decreaseSleep = () => {
+    dispatch(setHoursOfSleep(hoursOfSleep - 0.5));
+  };
+
+const handleInputChange = (e) => {
+  const inputValue = e.target.value;
+  
+  if (inputValue === "") {
+    dispatch(setHoursOfSleep("")); 
+    return;
+  }
+    
+  const numericValue = parseFloat(inputValue);
+
+   if (!isNaN(numericValue) && numericValue >= 0 && numericValue <= 23) {
+      dispatch(setHoursOfSleep(numericValue)); 
+    }
+    else{
+      dispatch(setHoursOfSleep("Sleep limit is 20 hours"))
+    }
+  }
+
+  const bedTime = dayjs(`2023-01-01T${wakeUpTime || "07:00"}`)
+  .subtract(hoursOfSleep, "hour")
+  .format("HH:mm");
+
   return (
-    <div className="bg-blue-950 h-screen flex flex-col items-center justify-center relative">
-      <div className="absolute top-[15%] text-4xl text-center">
-        Set up sleep schedule
-      </div>
-  
-      <div className="absolute top-[32%] flex flex-col items-center text-center gap-4 ">
-        <div className="text-xl">Choose hours of sleep</div>
-  
-        <div className="flex flex-row items-center ">
-          <Button onClick={decreaseSleep}>
-            <Minus />
-          </Button>
-          <Input
-            className="text-center py-8 "
-            type="number"
-            value={hoursOfSleep} // Koppla värdet till Redux state
-            onChange={handleInputChange} // Uppdatera Redux state vid inmatning
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div className="bg-white h-screen flex flex-col items-center justify-center relative">
+        <div className="absolute top-[10%] text-4xl text-center">
+          Set up sleep schedule
+        </div>
+        <div className="absolute top-[25%] text-xl text-center">
+          You need to go to bed at:
+          <div className="py-6" >{bedTime}</div>
+          
+          </div>
+
+        <div className="absolute top-[45%] flex flex-col items-center text-center gap-4 ">
+          <div className="text-xl">Choose hours of sleep</div>
+    
+          <div className="flex flex-row items-center ">
+            <Button className="py-6 px-6" onClick={decreaseSleep} disabled={hoursOfSleep==0} >
+              <Minus />
+            </Button>
+            <Input
+              className="text-center py-6 px-10 mx-4 "
+              type="number"
+              value={hoursOfSleep}
+              onChange={handleInputChange}
+            />
+            <Button className="py-6 px-6" onClick={increaseSleep} disabled={hoursOfSleep==20}>
+              <Plus />
+            </Button>
+          </div>
+        </div>
+    
+        {/* MobileTimePicker */}
+        <div className="flex flex-col items-center text-center absolute bottom-[25%]">
+          <h2 className="mb-4">Choose wake up time</h2>
+          <MobileTimePicker
+            value={dayjs(`2023-01-01T${wakeUpTime || "07:00"}`)}
+            onChange={handleTimeChange}
           />
-          <Button onClick={increaseSleep}>
-            <Plus />
-          </Button>
         </div>
       </div>
-  
-      {/* TimePicker */}
-      <div className="flex flex-col items-center text-center mt-[30%]">
-        <h2 className="mb-4">Choose wake up time</h2>
-        <TimePicker
-          onChange={handleTimeChange} // Uppdatera Redux state vid ändring
-          value={wakeUpTime} // Visa värdet från Redux state
-          use12Hours={false}
-          cellHeight={40}
-          className="time-wheel"
-          placeHolder="Choose wake up time"
-          cancelButtonText="Cancel"
-          saveButtonText="Save"
-          onFocus={handleTimePickerFocus} // Inaktivera scrollning
-          onBlur={handleTimePickerBlur}
-        />
-      </div>
-    </div>
+    </LocalizationProvider>
   );
 }
 
