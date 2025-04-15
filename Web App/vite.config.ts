0b497da0a6598a7c8f95d3f016e166fad48c5884
JonathanVarild <jonathan.varild@gmail.com>
2025-04-15 12:23:41 +0200

    Added React proxy for backend in local development

--- Web App/vite.config.ts
+++ Web App/vite.config.ts
@@ -39,4 +39,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+		port: 8080,
+		proxy: {
+			"/api": {
+				target: "http://localhost:3000",
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
 })
