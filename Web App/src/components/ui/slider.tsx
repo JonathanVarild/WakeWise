b4dd8c00a81f5e9177a86c0396bc810ea1ada075
Hazofinho <arvinkuna@hotmail.com>
2025-04-16 14:27:39 +0200

    Changed the look of the slider

--- Web App/src/components/ui/slider.tsx
+++ Web App/src/components/ui/slider.tsx
@@ -37,8 +37,8 @@ function Slider({
     >
       <SliderPrimitive.Track
         data-slot="slider-track"
-        className={cn(
-          "bg-muted relative grow overflow-hidden rounded-full data-[orientation=horizontal]:h-1.5 data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-1.5"
+        className={cn( // Modify 'data-[orientation=horizontal]:h-1' to change width of sliders
+          "bg-muted relative grow overflow-hidden rounded-full data-[orientation=horizontal]:h-1 data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-1.5"
         )}
       >
         <SliderPrimitive.Range
@@ -51,7 +51,7 @@ function Slider({
       {Array.from({ length: _values.length }, (_, index) => (
         <SliderPrimitive.Thumb
           data-slot="slider-thumb"
-          key={index}
+          key={index}                                     // 'size-4' Changes cricle size
           className="border-primary bg-background ring-ring/50 block size-4 shrink-0 rounded-full border shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50"
         />
       ))}
