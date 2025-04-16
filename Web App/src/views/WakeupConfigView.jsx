a50f890aa394e8ec4d858e35ae53889aeba6bda6
Hazofinho <arvinkuna@hotmail.com>
2025-04-16 14:28:20 +0200

    Added percentage view on 'Brightness' and 'Volume' sliders

--- Web App/src/views/WakeupConfigView.jsx
+++ Web App/src/views/WakeupConfigView.jsx
@@ -6,44 +6,44 @@ import { useState } from "react";
 
 function WakeupConfigView(props) {
     const [showLightning, setShowLightning] = useState(false);
+    const [volume, setVolume] = useState([30]);
+    const [brightness, setBrightness] = useState([30]);
 
     return (
-        <div className="flex flex-col gap-12 p-8">
+        <div className="flex flex-col gap-10 p-8">
 
             {/* Always Show */}
-            <div className="flex flex-col">
-                <label className="text-base font-semibold mb-4 text-left">Volume</label>
-                <Slider defaultValue={[30]} max={100} step={5} />
+            <div className="flex flex-col text-base font-semibold" >
+                <label className="mb-[4%]">Volume</label>
+                <Slider className="w-[60%]" defaultValue={[30]} max={100} step={5} value={volume} onValueChange={setVolume} />
+                <label className="ml-[22%] mt-[4%]">{volume[0]}%</label>
             </div>
 
-            <div className="flex flex-col">
-                <label className="text-base font-semibold mb-4 text-left">Wakeup duration</label>
-                <Slider defaultValue={[30]} max={100} step={5} />
+            <div className="flex flex-col text-base font-semibold">
+                <label className="mb-[4%]">Wakeup duration</label>
+                <Slider className="w-[50%]" defaultValue={[30]} max={100} step={5} />
             </div>
 
             {/* Toggle */}
-            <div className="flex flex-col">
-                <label className="text-base font-semibold mb-4 text-left">Enable Ambient Lightning</label>
+            <div className="flex flex-col text-base font-semibold">
+                <label className="mb-[4%]">Enable Ambient Lightning</label>
                 <div className="flex justify-left ">
-                    <Switch 
-                        size="m"
-                        checked={showLightning}
-                        onCheckedChange={setShowLightning}
-                    />
+                    <Switch size="m" checked={showLightning} onCheckedChange={setShowLightning} />
                 </div>
             </div>
             
             {/* Conditionally show brightness & color sliders */}
             {showLightning && (
                 <>
-                    <div className="flex flex-col">
-                        <label className="text-base font-semibold mb-4 text-left">Brightness</label>
-                        <Slider defaultValue={[30]} max={100} step={5} />
+                    <div className="flex flex-col text-base font-semibold">
+                        <label className="mb-[4%]">Brightness</label>
+                        <Slider className="w-[50%]" defaultValue={[30]} max={100} step={2} value={brightness} onValueChange={setBrightness} />
+                        <label className="ml-[22%] mt-[4%]">{brightness[0]}%</label>
                     </div>
 
-                    <div className="flex flex-col">
-                        <label className="text-base font-semibold mb-4 text-left">Colour</label>
-                        <Slider defaultValue={[30]} max={100} step={5} />
+                    <div className="flex flex-col text-base font-semibold">
+                        <label className="mb-[4%]">Colour</label>
+                        <Slider className="w-[50%]" defaultValue={[30]} max={100} step={5} />
                     </div>
                 </>
             )}
