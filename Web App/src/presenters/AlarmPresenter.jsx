554b412d8f93d33b2d87480faaf7ec42d61fa3af
floralindberg <floral@kth.se>
2025-04-25 10:19:45 +0200

    Update AlarmPresenter.jsx

--- Web App/src/presenters/AlarmPresenter.jsx
+++ Web App/src/presenters/AlarmPresenter.jsx
@@ -4,65 +4,80 @@ import { useSelector, useDispatch } from "react-redux";
 import dayjs from "dayjs";
 import { useState } from "react";
 
-
 function AlarmPresenter() {
+	const [errorMessage, setErrorMessage] = useState("");
 
-  const [errorMessage, setErrorMessage] = useState("")
+	const dispatch = useDispatch();
 
-  const dispatch = useDispatch();
+	const hoursOfSleep = useSelector((state) => state.interface.hoursOfSleep);
+	const wakeUpTime = useSelector((state) => state.interface.wakeUpTime);
 
-  const hoursOfSleep = useSelector((state) => state.interface.hoursOfSleep);
-  const wakeUpTime = useSelector((state) => state.interface.wakeUpTime);
+	const handleTimeChangeACB = (newValue) => {
+		if (newValue) {
+			const formattedTime = dayjs(newValue).format("HH:mm");
+			dispatch(setWakeUpTime(formattedTime));
+		}
+	};
 
-  const handleTimeChangeACB = (newValue) => {
-    if (newValue) {
-      const formattedTime = dayjs(newValue).format("HH:mm");
-      dispatch(setWakeUpTime(formattedTime));
-    }
-  };
+	const increaseSleepACB = () => {
+		dispatch(setHoursOfSleep(hoursOfSleep + 0.5));
+	};
 
-  const increaseSleepACB = () => {
-    dispatch(setHoursOfSleep(hoursOfSleep + 0.5));
-  };
+	const decreaseSleepACB = () => {
+		dispatch(setHoursOfSleep(hoursOfSleep - 0.5));
+	};
 
-  const decreaseSleepACB = () => {
-    dispatch(setHoursOfSleep(hoursOfSleep - 0.5));
-  };
+	const handleInputChangeACB = (inputValue) => {
+		console.log("Received inputValue:", inputValue);
 
-  const handleInputChangeACB = (inputValue) => {
-    console.log("Received inputValue:", inputValue);
-  
-    if (inputValue === "") {
-      dispatch(setHoursOfSleep("")); // Sätt till tomt om input är tomt
-      return;
-    }
-  
-    const numericValue = parseFloat(inputValue);
-    if (!isNaN(numericValue) && numericValue >= 0 && numericValue <= 20) {
-      dispatch(setHoursOfSleep(numericValue)); // Uppdatera Redux state
-      setErrorMessage("")
-    } else {
-      setErrorMessage("Value can't exceed 20 hours")
-    }
-  };
+        // Sätter en tom sträng. Detta görs för att värdet ska uppdateras på skärmen, kommer inte gå att sparas om den är tom men annars visas det gamla värdet.
+		if (inputValue === "") {
+			dispatch(setHoursOfSleep(""));
+			return;
+		}
 
-  const bedTime = dayjs(`2023-01-01T${wakeUpTime || "07:00"}`)
-  .subtract(hoursOfSleep, "hour")
-  .format("HH:mm");
+		const numericValue = parseFloat(inputValue);
+		if (!isNaN(numericValue) && numericValue >= 0 && numericValue <= 20) {
+			dispatch(setHoursOfSleep(numericValue));
+			setErrorMessage("");
+		}
+		else if (numericValue < 0) {
+            dispatch(setHoursOfSleep(numericValue));
+			setErrorMessage("Sleeping hours can't be negative");
+		}
+		else if (numericValue > 20) {
+			dispatch(setHoursOfSleep(numericValue));
+			setErrorMessage("Sleeping hours can't be over 20 hours");
+		}
+	};
 
-  return <AlarmView bedTime={bedTime} 
-                    hoursOfSleep={hoursOfSleep} 
-                    wakeUpTime={wakeUpTime} 
-                    handleTimeChange={handleTimeChangeACB} 
-                    increaseSleep={increaseSleepACB} 
-                    decreaseSleep={decreaseSleepACB}
-                    handleInputChange={handleInputChangeACB} 
-                    errorMessage={errorMessage}
-                    //numericValue={numericValue}
-                    //inputValue={inputValue}
-                    //formattedTime={formattedTime}
-                    //dispatch={dispatch}
-                  />;
+	const saveAlarmData = (state) => {
+		const savedWakeUpTime = wakeUpTime;
+		const savedHoursOfSleep = hoursOfSleep;
+		const savedBedtime = bedtime;
+		console.log(savedWakeUpTime);
+		console.log(savedHoursOfSleep);
+		console.log(savedBedtime);
+		//state.interface.saveAlarm();
+	};
+
+	const bedtime = dayjs(`2023-01-01T${wakeUpTime || "07:00"}`)
+		.subtract(hoursOfSleep, "hour")
+		.format("HH:mm");
+
+	return (
+		<AlarmView
+			bedtime={bedtime}
+			hoursOfSleep={hoursOfSleep}
+			wakeUpTime={wakeUpTime}
+			handleTimeChange={handleTimeChangeACB}
+			increaseSleep={increaseSleepACB}
+			decreaseSleep={decreaseSleepACB}
+			handleInputChange={handleInputChangeACB}
+			errorMessage={errorMessage}
+			saveAlarmData={saveAlarmData}
+		/>
+	);
 }
 
 export default AlarmPresenter;
