3f628543a98a24575e76fc826b82fd3e99432147
JonathanVarild <jonathan.varild@gmail.com>
2025-05-06 16:10:13 +0200

    Code cleanup in screen time subsettings

--- Web App/src/lib/constants.ts
+++ Web App/src/lib/constants.ts
@@ -1,15 +1,13 @@
 export const soundOptions = [
-  { value: "Sound1", label: "Sound1" },
-  { value: "Sound2", label: "Sound2" },
-  { value: "Sound3", label: "Sound3" },
-  { value: "Sound4", label: "Sound4" },
-  { value: "Sound5", label: "Sound5" },
+	{ value: "Sound1", label: "Sound1" },
+	{ value: "Sound2", label: "Sound2" },
+	{ value: "Sound3", label: "Sound3" },
+	{ value: "Sound4", label: "Sound4" },
+	{ value: "Sound5", label: "Sound5" },
 ];
 
 export const alertOptions = [
-  { value: "vibrate", label: "Vibrate Only" },
-  { value: "sound", label: "Sound Only" },
-  { value: "sound_vibrate", label: "Sound & Vibrate" },
-  { value: "silent", label: "Silent" },
-  { value: "led", label: "LED Flash Only" },
+	{ value: "clockphone", label: "Clock & Phone" },
+	{ value: "clock", label: "Clock" },
+	{ value: "phone", label: "Phone" },
 ];
--- Web App/src/model/modules/screentime.js
+++ Web App/src/model/modules/screentime.js
@@ -1,43 +1,32 @@
 import { createReduxModule } from "../ReduxHelpers";
 
 const module = createReduxModule("screentime", {
-  pre_sleep_min: 0,
-  post_sleep_min: 0,
-  alertType: "notificaton",
+	pre_sleep_min: null,
+	post_sleep_min: null,
+	alertType: null,
 });
 export default module;
 
-export const setAlertType = module.addReducer(
-  "setAlertType",
-  (state, action) => {
-    state.alertType = action.payload;
-  }
-);
-
-export const setPreSleep = module.addReducer("setPreSleep", (state, action) => {
-  state.pre_sleep_min = action.payload;
+export const setAlertType = module.addReducer("setAlertType", (state, action) => {
+	state.alertType = action.payload;
 });
 
-export const setPostSleep = module.addReducer(
-  "setPostSleep",
-  (state, action) => {
-    state.post_sleep_min = action.payload;
-  }
-);
+export const setPreSleep = module.addReducer("setPreSleep", (state, action) => {
+	state.pre_sleep_min = action.payload;
+});
 
-export const getScreentimeSettings = module.addFetcher(
-  "getScreentimeSettings",
-  "/api/settings/getscreentime",
-  {
-    onSuccess: (state, action) => {
-      state.pre_sleep_min = action.payload.pre_sleep_min;
-      state.post_sleep_min = action.payload.post_sleep_min;
-      state.alertType = action.payload.alertType;
-    },
-  }
-);
+export const setPostSleep = module.addReducer("setPostSleep", (state, action) => {
+	state.post_sleep_min = action.payload;
+});
 
-export const setScreentimeSettings = module.addFetcher(
-  "setScreentimeSettings",
-  "/api/settings/setscreentime"
-);
+export const getScreentimeSettings = module.addFetcher("getScreentimeSettings", "/api/settings/getscreentime", {
+	onSuccess: (state, action) => {
+		console.log("getScreentimeSettings", action.payload);
+		
+		state.pre_sleep_min = action.payload.pre_sleep_min;
+		state.post_sleep_min = action.payload.post_sleep_min;
+		state.alertType = action.payload.alertType;
+	},
+});
+
+export const setScreentimeSettings = module.addFetcher("setScreentimeSettings", "/api/settings/setscreentime");
