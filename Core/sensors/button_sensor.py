9aa105505f4628d25761d335b3d8155a092e307a
JonathanVarild <jonathan.varild@gmail.com>
2025-05-11 12:27:56 +0200

    Implemented button sensor

--- Core/sensors/button_sensor.py
+++ Core/sensors/button_sensor.py
@@ -1,24 +1,18 @@
 import threading
 import time
-#from gpiozero import Button
-
-
-
+from gpiozero import Button
 
 class ButtonSensor:
     def __init__(self):
         self.lock = threading.Lock()
         self.thread = threading.Thread(target=self.worker, daemon=True)
         self.prefix = "Button Sensor"
-        self.listeners = []
-        # self.button1 = Button(6) #pin31
-        # self.button2 = Button(5) #pin29
-        # self.button1.when_pressed = when_pressed
-        # self.button2.when_pressed = when_pressed
-
-    # def when_pressed():
-    #     print("Button pressed!")
-
+        self.listeners_button1 = []
+        self.listeners_button2 = []
+        self.button1 = Button(6) #pin31
+        self.button2 = Button(5) #pin29
+        self.pressed_button1 = self.button1.is_pressed
+        self.pressed_button2 = self.button2.is_pressed
 
     def print(self, *args):
         print(f"[{self.prefix}]", *args)
@@ -35,21 +29,37 @@ class ButtonSensor:
             
     def read_sensor(self):
         with self.lock:
-            btn1State = False # TODO: Replace with actual sensor reading.
-            btn2State = False # TODO: Replace with actual sensor reading.
+            new_btn_1 = self.button1.is_pressed
+            new_btn2 = self.button2.is_pressed
             
-            if btn1State and btn2State:
-                for listener in self.listeners:
-                    listener(btn1State, btn2State)
+            if new_btn_1 and new_btn_1 != self.pressed_button1:
+                for listener in self.listeners_button1:
+                    listener(new_btn_1)
+                    
+            if new_btn2 and new_btn2 != self.pressed_button2:
+                for listener in self.listeners_button2:
+                    listener(new_btn2)
+                    
+            self.pressed_button1 = new_btn_1
+            self.pressed_button2 = new_btn2
             
-    def get_data(self):
+    def get_button1(self):
         with self.lock:
-            return self.bed_occupancy
+            return self.button1.is_pressed
         
-    def add_listener(self, listener):
+    def get_button2(self):
         with self.lock:
-            if listener not in self.listeners:
-                self.listeners.append(listener)
+            return self.button2.is_pressed
+        
+    def add_listener_button1(self, listener):
+        with self.lock:
+            if listener not in self.listeners_button1:
+                self.listeners_button1.append(listener)
+                
+    def add_listener_button2(self, listener):
+        with self.lock:
+            if listener not in self.listeners_button2:
+                self.listeners_button2.append(listener)
             
 # Create singleton instance of ButtonSensor
 button_sensor = ButtonSensor()
