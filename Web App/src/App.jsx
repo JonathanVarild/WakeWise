ac9f88a94489a65aff03ebcedcafc4b5134e9ba8
JonathanVarild <jonathan.varild@gmail.com>
2025-05-13 22:07:14 +0200

    Removed unused imports and fixed incorrect casing in import

--- Web App/src/App.jsx
+++ Web App/src/App.jsx
@@ -1,11 +1,5 @@
-import {
-  TAB_ALARM,
-  TAB_STATISTICS,
-  TAB_RECORDINGS,
-  TAB_SETTINGS,
-} from "./model/modules/navigation";
+import { TAB_ALARM, TAB_STATISTICS, TAB_RECORDINGS, TAB_SETTINGS } from "./model/modules/navigation";
 import { useSelector } from "react-redux";
-import PageView from "./views/PageView";
 import NavbarPresenter from "./presenters/NavbarPresenter";
 import AlarmPresenter from "./presenters/AlarmPresenter";
 import StatisticsChartPresenter from "./presenters/StatisticsChartPresenter";
@@ -15,63 +9,50 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { reauthenticateUser } from "./model/modules/authentication";
 import RecordingsPresenter from "./presenters/RecordingsPresenter";
-import SoundPresenter from "./presenters/SoundPresenter";
-import SettingsLightPresenter from "./presenters/SettingsLightPresenter";
-import ScreenTimePresenter from "./presenters/ScreenTimePresenter";
-import RoutinesPresenter from "./presenters/RoutinesPresenter";
-import { SummaryPresenter } from "./presenters/summaryPresenter";
-import dayjs from "dayjs"; // Installera dayjs om det inte redan finns
+import { SummaryPresenter } from "./presenters/SummaryPresenter";
+import dayjs from "dayjs";
 import { useState } from "react";
-import { SummaryView } from "./views/SummaryView";
-
 
 function App() {
-  const dispatch = useDispatch();
-  const authenticated = useSelector(
-    (state) => state.authentication.authenticatedAs
-  );
-  const activeTab = useSelector((state) => state.navigation.navigationTab);
+	const dispatch = useDispatch();
+	const authenticated = useSelector((state) => state.authentication.authenticatedAs);
+	const activeTab = useSelector((state) => state.navigation.navigationTab);
 
-  const [showSummary, setShowSummary] = useState(true); 
+	const [showSummary, setShowSummary] = useState(true);
 
+	// Define the pages for each tab
+	const pages = [];
+	pages[TAB_ALARM] = <AlarmPresenter />;
+	pages[TAB_STATISTICS] = <StatisticsChartPresenter />;
+	pages[TAB_RECORDINGS] = <RecordingsPresenter />;
+	pages[TAB_SETTINGS] = <SettingsPresenter />;
 
-  // Define the pages for each tab
-  const pages = [];
-  pages[TAB_ALARM] = <AlarmPresenter />;
-  pages[TAB_STATISTICS] = <StatisticsChartPresenter />;
-  pages[TAB_RECORDINGS] = <RecordingsPresenter />;
-  pages[TAB_SETTINGS] = <SettingsPresenter />;
+	useEffect(() => {
+		if (!authenticated) {
+			dispatch(reauthenticateUser());
+		}
+	}, [dispatch]);
 
-  useEffect(() => {
-    if (!authenticated) {
-      dispatch(reauthenticateUser());
-    }
-  }, [dispatch]);
+	const currentHour = dayjs().hour();
 
-  const currentHour = dayjs().hour();
+	function closeSummaryACB() {
+		setShowSummary(false);
+	}
 
-
-  function closeSummaryACB(){
-    setShowSummary(false);
-  }
-
-  if (authenticated) {
-   
-    if(showSummary && currentHour > 5 && currentHour < 18){
-      return <SummaryPresenter closeSummary={closeSummaryACB} />;
-
-    } else {
-    return (
-      <>
-        {pages[activeTab]}
-        <NavbarPresenter />
-      </>
-    );
-  }
-
-  } else {
-    return <AuthenticatePresenter />;
-  }
+	if (authenticated) {
+		if (showSummary && currentHour > 5 && currentHour < 18) {
+			return <SummaryPresenter closeSummary={closeSummaryACB} />;
+		} else {
+			return (
+				<>
+					{pages[activeTab]}
+					<NavbarPresenter />
+				</>
+			);
+		}
+	} else {
+		return <AuthenticatePresenter />;
+	}
 }
 
 export default App;
