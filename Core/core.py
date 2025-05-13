603b1fc2cf007c4c3a8c338deaa265701a5aed5e
Hazofinho <arvinkuna@hotmail.com>
2025-05-13 16:46:17 +0200

    Implemented display logic

--- Core/core.py
+++ Core/core.py
@@ -1,37 +1,36 @@
 import time
 import pygame
-from utils.configuration_manager import configuration_manager
+#from utils.configuration_manager import configuration_manager
 
-from sensors.bed_sensor import bed_sensor
-from sensors.button_sensor import button_sensor
-from sensors.environment_sensor import environment_sensor
-from sensors.phone_sensor import phone_sensor
+#from sensors.bed_sensor import bed_sensor
+#from sensors.button_sensor import button_sensor
+#from sensors.environment_sensor import environment_sensor
+#from sensors.phone_sensor import phone_sensor
 
-from services.alarms import alarm_service
-from services.metrics_tracker import metrics_tracker_service
-from services.sleep import sleep_service
-from services.phone_tracker import phone_tracker_service
-from services.recordings import recordings_service
+#from services.alarms import alarm_service
+#from services.metrics_tracker import metrics_tracker_service
+#from services.sleep import sleep_service
+from services.display import display_service
 
-from controllers.led_controller import led_controller
+#from controllers.led_controller import led_controller
+from controllers.display_controller import display_controller
 
-led_controller.set_led(False)
+#led_controller.set_led(False)
 
 # Start utility threads.
-configuration_manager.start()
+#configuration_manager.start()
 
 # Start sensor threads.
-# bed_sensor.start()
-# button_sensor.start()
-# environment_sensor.start()
-# phone_sensor.start()
+#bed_sensor.start()
+#button_sensor.start()
+#environment_sensor.start()
+#phone_sensor.start()
 
 # Start services threads.
 #alarm_service.start()
 # metrics_tracker.start()
 #sleep_service.start()
-#phone_tracker_service.start()
-#recordings_service.start()
+display_service.start()
 
 # Wait for threads to finish.
 try:
---
+++ Core/utils/TextLib.py
@@ -0,0 +1,14 @@
+# Library for pre-defined/reusable text through the project
+
+from .display_text_module import Text
+
+# --- Group Members ---
+group_members = {
+    #"arvin": Text("Arvin.K", anchor="center"),
+    "flora": Text("Flora.L", anchor="bottom-center"),
+    "johan": Text("Johan.L", anchor="bottom-left"),
+    "philip": Text("Philip.D", anchor="bottom-right"),
+    "yunzhu": Text("Yunzhu.T", anchor="top-left"),
+    "jonathan": Text("Jonathan.V", anchor="top-right")
+}
+# ---------------------
