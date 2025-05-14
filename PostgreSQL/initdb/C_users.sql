d70d40ada22980e0b70e70ee0cd34a7414c0930b
JonathanVarild <jonathan.varild@gmail.com>
2025-05-14 09:03:59 +0200

    Removed clock_name from users table

--- PostgreSQL/initdb/C_users.sql
+++ PostgreSQL/initdb/C_users.sql
@@ -2,7 +2,6 @@ CREATE TABLE users (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   username VARCHAR NOT NULL UNIQUE,
   password_hash TEXT NOT NULL,
-  clock_name VARCHAR,                       
   is_admin BOOLEAN NOT NULL DEFAULT FALSE,
   created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
